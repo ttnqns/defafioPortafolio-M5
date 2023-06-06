0c85ba78f651149970099e9717acfd6659bdab35
ttnqns <tatianaqog@gmail.com>
2023-06-06 17:55:04 -0400

    Create project

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/projects'
+  get 'pages/contact'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  root "pages#home"
+end
